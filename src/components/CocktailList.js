371eb7c582f9406819c7e2bcff79aeb4ff8ab2fe
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-08 19:26:38 +0200

    added basic context setup

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+
+  // loading
+  if (loading) {
+    return <Loading />;
+  }
+
+  // the cocktails array is empty
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        no cocktails matched your search criteria!
+      </h2>
+    );
+  }
+
+  // there are cocktails
   return (
     <div>
       <h2>cocktail list component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default CocktailList
+export default CocktailList;
