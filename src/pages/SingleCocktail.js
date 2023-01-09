d0732a71e8cade4d1723145b4d4b617ab5b1db1d
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-09 19:17:25 +0200

    setup the specific cocktail fetch

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,14 +1,75 @@
-import React from 'react'
-import Loading from '../components/Loading'
-// import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import React from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState({});
+
+  // fetch that specific cocktail
+  const fetchCocktail = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${id}`);
+      const data = await response.json();
+      const { drinks } = data;
+
+      // in case the drink is there
+      if (drinks) {
+        // giving new aliases(names) to the destructured values
+        const {
+          strDrink: name,
+          strDrinkThumb: image,
+          strAlcoholic: info,
+          strCategory: category,
+          strGlass: glass,
+          strInstructions: instructions,
+          strIngredient1,
+          strIngredient2,
+          strIngredient3,
+          strIngredient4,
+          strIngredient5,
+        } = drinks[0];
+
+        // setting up the cocktail
+        const newCocktail = {
+          name,
+          image,
+          info,
+          category,
+          glass,
+          instructions,
+          ingrediants: [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ],
+        };
+        setCocktail(newCocktail);
+      } else {
+        setCocktail(null);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchCocktail();
+  }, [id]);
+
+  console.log(cocktail);
   return (
     <div>
       <h2>single cocktail page </h2>
     </div>
-  )
-}
+  );
+};
 
-export default SingleCocktail
+export default SingleCocktail;
